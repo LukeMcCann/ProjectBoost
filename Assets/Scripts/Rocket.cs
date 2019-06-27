14b1576593df05af32a518887854a2c00d6d4568
LukeMcCann <developer-lukemccann@outlook.com>
2019-06-27 18:36:03 +0100

    Adds dummy rocket control script

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Rocket
+
+ * Author: Luke McCann
+ * 
+ * handles rocket controls.
+ */ 
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space)) // Can thrust whilst rotating
+        {
+            EngageThrusters();
+        }
+
+        if (Input.GetKey(KeyCode.A)&& !Input.GetKey(KeyCode.D))
+        {
+            RotateLeft();
+        }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            RotateRight();
+        }
+    }
+
+    private void EngageThrusters()
+    {
+        print("Thrust Active");
+    }
+
+    private void RotateLeft()
+    {
+        print("Rotating Left");
+    }
+
+    private void RotateRight()
+    {
+        print("Rotating Right");
+    }
+}

5a7d9278551fab104e30e833d55c4394a5b148f7
LukeMcCann <developer-lukemccann@outlook.com>
2019-06-29 20:36:55 +0100

    Adds oscillation mechanic

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -9,17 +9,20 @@ public class Oscillator : MonoBehaviour
     [SerializeField] Vector3 movementVector;
 
     // TODO: remove from inspector later
-    float movementFactor;
+    [Range(-1, 1)][SerializeField] float movementFactor;
+
+    private Vector3 startingPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
     }
 }
---
+++ README.md
@@ -0,0 +1,17 @@
+Project Boost
+
+Author: Luke McCann
+
+A Simple rocket game like that of the classic game "Thrust".
+
+Note: 
+this project does not utilise best practices. This project was built whilst getting to grips
+with Unity specific methods and techniques via Udemy. 
+
+To Play:
+
+. Use spacebar to engage thrusters
+. A and D to rotate
+
+The aim is to find and reach the goal block indicated by the flag before
+your fuel runs out!
